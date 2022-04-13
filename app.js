890960725d68959edc0e2a1d95c57f869ee7642d
Brenton Ring <brentonring@Brentons-MacBook-Air.local>
2022-04-13 18:59:06 -0500

    add ring colors

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // Trivia question API
 async function getTriviaQuestions() {
     let response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
-    let questions = await response.json()
-    console.log(questions)
+    let returns = await response.json()
+    let resultsArray = await returns.results
+
+    console.log(resultsArray);
+    // console.log(returns.results[0].question)
+
+    for (let i = 0; i < resultsArray.length; i++) {
+        let questions = resultsArray[i].question;
+        console.log(questions);
+        let questionBox = document.getElementById('questionbox');
+        questionBox.append(questions)
+    }
 }
 getTriviaQuestions()
 // Choose answer with mouse click
@@ -12,6 +22,7 @@ getTriviaQuestions()
 // Turn ring green with correct answer, wrong if incorrect
 
 // Give user ten points for correct answer, zero for incorrect
+// score +=10
 
 // Keep track of point total
 
