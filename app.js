a071c9c2fb75f0ee8da7f96037060d5b6ee89797
Brenton Ring <brentonring@Brentons-MacBook-Air.local>
2022-04-16 12:12:19 -0500

    refactored and added functionality

--- app.js
+++ app.js
@@ -44,33 +44,50 @@ async function getTrivia() {
     }
     console.log(incorrectArray3)
 
-    function appendTrivia() {
+    function appendTrivia(index) {
         let questionBox = document.getElementById('questionbox');
-        questionBox.append(questionArray[0]);
+        questionBox.append(questionArray[index]);
     
         let correctBox = document.getElementById('answer1');
-        correctBox.append(correctArray[0]);
+        correctBox.append(correctArray[index]);
     
         let incorrectBox1 = document.getElementById('answer2');
-        incorrectBox1.append(incorrectArray1[0]);
+        incorrectBox1.append(incorrectArray1[index]);
     
         let incorrectBox2 = document.getElementById('answer3');
-        incorrectBox2.append(incorrectArray2[0]);
+        incorrectBox2.append(incorrectArray2[index]);
     
         let incorrectBox3 = document.getElementById('answer4');
-        incorrectBox3.append(incorrectArray3[0]);
+        incorrectBox3.append(incorrectArray3[index]);
     }
-    appendTrivia()    
+    appendTrivia(0)
 }
 getTrivia()
 
 
 // Choose answer with mouse click
-const answerBoxes = document.getElementsByClassName('answers')
-async function clicks() {
-    answerBoxes.addEventListener("click")
+let correctAnswerBox = document.getElementById('answer1')
+const iAnswerBoxes = document.getElementsByClassName('iAnswers')
+
+window.onload = () => {
+    correct = () => {
+        correctAnswerBox.classList.add('correct')
+        addPoints()
+        document.getElementById('ring').classList.add('ringCorrect')
+    }
+    
+    incorrect = (evt) => {
+        evt.target.classList.add('incorrect')
+        document.getElementById('ring').classList.add('ringIncorrect')
+        correctAnswerBox.classList.add('correct')
+    }
+    for(let i=0; i<iAnswerBoxes.length; i++) {
+        iAnswerBoxes[i].addEventListener("click", incorrect, false)
+    }
+    correctAnswerBox = document.getElementById('answer1')
+    correctAnswerBox.addEventListener("click", correct, false)
 }
-let clickTimeout = setTimeout(clicks())
+
 // Choose answer with arrow keys
 
 // Turn ring green with correct answer, wrong if incorrect
@@ -79,4 +96,10 @@ let clickTimeout = setTimeout(clicks())
 // score +=10
 
 // Keep track of point total
+let pts = 0
 
+addPoints = () => {
+    pts += 10
+    let pointBox = document.getElementById('points')
+    pointBox.textContent = pts
+}
